d4c2488ad6ba9b88eb0b9acc00ba238b73fa0221
Monica Pardo <moonyruna3@gmail.com>
2024-06-11 16:48:01 +0200

    Applied ternary operation

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,29 @@
 
  function PokemonCard () {
-  return (
 
+  const pokemon = pokemonList[0]
+
+  return (
+    
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur pokemon" />
+      {pokemon.imgSrc? <img src={pokemon.imgSrc} alt="Bulbasaur pokemon" />: <p>???</p>}
       <figcaption>Bulbasaur</figcaption>
 
     </figure>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: 
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+  },
+  {
+    name: "meow",
+    imgSrc: ""
+  },
+];
+
+
 export default PokemonCard

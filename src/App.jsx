83dcb350965ec5268397fdbf9cc2badfbd2c55f6
Monica Pardo <moonyruna3@gmail.com>
2024-06-13 16:56:03 +0200

    finish ,need to make the bonus

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,57 @@ import PokemonCard from './components/PokemonCard'
 
 
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc: 
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      pokemonIndex: 1,
-    },
-    {
-      name: "meow",
-      imgSrc: ""
-    },
-    {
-      name: "",
-      imgSrc: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fc8.alamy.com%2Fcomp%2FH86XXY%2Fcute-fox-cartoon-H86XXY.jpg&tbnid=CFFibJBpp75c6M&vet=10CBUQxiAoBWoXChMIoKmFz9PYhgMVAAAAAB0AAAAAEAc..i&imgrefurl=https%3A%2F%2Fwww.alamy.com%2Fstock-photo%2Fpokemon.html%3Fcutout%3D1&docid=KlSIF5fRpE-Q3M&w=1111&h=1390&itg=1&q=pokemon%20pictures&ved=0CBUQxiAoBWoXChMIoKmFz9PYhgMVAAAAAB0AAAAAEAc"
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
+
+    const [pokemonIndex, setPokemonIndex] = useState(0);
+
+    const handleNextClick = () => {
+      if (pokemonIndex < pokemonList.length -1) {
+        setPokemonIndex(pokemonIndex + 1)
+      }
     }
-  ];
+
+    const handlePreviusClick = () => {
+      if (pokemonIndex > 0) {
+        setPokemonIndex(pokemonIndex - 1)
+      }
+    }
+
+  
 
   return (
     <>
-    <PokemonCard pokemon={pokemonList[0]}></PokemonCard>
-    <PokemonCard pokemon={pokemonList[1]}></PokemonCard>
-    <PokemonCard pokemon={pokemonList[2]}></PokemonCard>
+    <PokemonCard pokemon={pokemonList[pokemonIndex]}></PokemonCard>
     
+    <button onClick={handleNextClick}>Next</button>
+    <button onClick={handlePreviusClick}>Previus</button>
      
     </>
   )

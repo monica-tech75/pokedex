ebc2c21288368887c5da8e5d370c91035e43e4fc
Monica Pardo <moonyruna3@gmail.com>
2024-06-16 10:01:49 +0200

    Create a Navbar component and move buttons to the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 export default function NavBar ({handleNextClick, handlePreviusClick}){
   return (
     <>
-
-<button onClick={handleNextClick}>Next</button>
 <button onClick={handlePreviusClick}>Previus</button>
+<button onClick={handleNextClick}>Next</button>
+
     </>
 
 
